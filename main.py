0b55d8e58c420d39fd76b7ed0edcfc59efb1d5a4
carsonbleh <142935274+carsonbleh@users.noreply.github.com>
2023-12-01 12:49:21 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+# This file was created by: Carson Bleh
+
+'''
+Final project: snake video game - recreation of google snake game
+Goals:
+Introduction and game end screen
+Ability to pause game
+Single player and two player mode
+Game ends when snake hits wall
+Gain points when snake eats apple
+'''
